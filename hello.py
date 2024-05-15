1e0e94775fc17f3668046143ec0b6cb2639e658f
Allan <121550986+a-ll-an@users.noreply.github.com>
2024-05-15 19:28:20 -0400

    remaining tasks

--- hello.py
+++ hello.py
@@ -1,2 +1,27 @@
+#Task 1
 print("Hello World")
+
+#Task 2
+x = input("What is your name? ")
+print(f"Hello {x}, it's nice to meet you")
+
+#Task 3
+a = int(input("Give me a number "))
+b = int(input("Give me another number "))
+
+sum = a+b
+difference = a-b
+product = a*b
+quotient = a / b
+
+print(f"The sum is {sum}")
+print(f"The difference is {difference}")
+print(f"The product is {product}")
+print(f"The quotient is {quotient}")
+
+#Task 4
+age = int(input("What is your age? "))
+
+print(f"Next year, you will be {age+1}!")
+
 
