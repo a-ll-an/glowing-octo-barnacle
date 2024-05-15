3afb4a7d66b190ba253641eee516c51deeb7815a
Allan <121550986+a-ll-an@users.noreply.github.com>
2024-05-15 19:43:30 -0400

    funky code

---
+++ funky_code.py
@@ -0,0 +1,36 @@
+char = "$"
+
+print("")
+
+print(f" {char*4}")
+print(f"{char} ")
+print(f"{char} ")
+print(f"{char} ")
+print(f" {char*4}")
+
+print("")
+
+print(f" {char*4}")
+print(f"{char}    {char}")
+print(f"{char}    {char}")
+print(f"{char}    {char}")
+print(f" {char*4}")
+
+print("")
+
+print(f"{char*3}")
+print(f"{char}  {char}")
+print(f"{char}   {char}")
+print(f"{char}  {char}")
+print(f"{char*3}")
+
+print("")
+
+print(f"{char*4}")
+print(f"{char} ")
+print(f"{char*4}")
+print(f"{char} ")
+print(f"{char*4}")
+
+print("")
+
