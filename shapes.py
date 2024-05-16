ce9b54efbbe367285b3b30eabe7edbe65281e54b
Allan <121550986+a-ll-an@users.noreply.github.com>
2024-05-15 20:03:40 -0400

    area of rectangle

---
+++ shapes.py
@@ -0,0 +1,13 @@
+#Calculate the area of the rectangle
+length = int(input("Please give me the length of the rectangle "))
+width = int(input("Please give me the width of the rectangle "))
+
+print(f"The area of the rectangle is {length*width}")
+
+import math
+
+#Area of the triangle
+
+
+
+
