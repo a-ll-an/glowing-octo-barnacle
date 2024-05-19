c1342d8ad9f7e6e2f521dd2e4f87c8e7c1dad4e5
Allan <121550986+a-ll-an@users.noreply.github.com>
2024-05-18 20:26:46 -0400

    area of triangle

--- shapes.py
+++ shapes.py
@@ -6,11 +6,19 @@ print(f"The area of the rectangle is {length*width}")
 
 import math
 
-#Area of the triangle
+#Area of the circle
 
 radius = int(input("Please give me the radius of the circle "))
 
 print(f"The area of the circle is {math.pi*(radius**2)}")
+
+#Area of a triangle
+base = int(input("please give me the base of the triangle: "))
+
+height = int(input("please give me the height of the triangle: "))
+
+print(f"The area of the triangle is {(base*height)/2} and is cool")
+
 
 
 
