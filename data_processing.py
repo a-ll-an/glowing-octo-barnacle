aa7ef8c84474db602fc36d1b890c55f1acdb6ee9
Allan <121550986+a-ll-an@users.noreply.github.com>
2024-05-22 20:49:15 -0400

    upper case and split methods

---
+++ data_processing.py
@@ -0,0 +1,7 @@
+sentence = input("Give me a sentence: ").upper()
+print(sentence)
+
+paragraph = input("Give me a paragraph: ").split()
+count = int(len(paragraph))
+print(f"The number of words in the paragraph is {count}")
+
