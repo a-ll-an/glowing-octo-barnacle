abfd80cd48b506c070d22c9ef8f21ee545ca60d1
Allan <121550986+a-ll-an@users.noreply.github.com>
2024-05-23 20:29:14 -0400

    exercise 3 and 4

--- data_processing.py
+++ data_processing.py
@@ -1,7 +1,19 @@
+#1 
+
 sentence = input("Give me a sentence: ").upper()
 print(sentence)
 
+#2
 paragraph = input("Give me a paragraph: ").split()
 count = int(len(paragraph))
 print(f"The number of words in the paragraph is {count}")
 
+#3
+string = input("Give me a string: ").isnumeric()
+print(string)
+
+#4 
+yuh = input("Give me a string with a's in it: ").replace("a","o")
+print(yuh)
+
+#5
